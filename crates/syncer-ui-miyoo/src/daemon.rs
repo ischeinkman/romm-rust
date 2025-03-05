ee35343c0d18e666bafc078466b919b0cbf2d125
ischeinkman <scheinkman.ilan@gmail.com>
2025-03-05 01:10:14 -0500

    Better errors for daemon manipulation

--- crates/syncer-ui-miyoo/src/daemon.rs
+++ crates/syncer-ui-miyoo/src/daemon.rs
@@ -1,5 +1,6 @@
 use std::io;
 
+use thiserror::Error;
 use tokio::{fs, process::Command};
 
 const SERVICE_INSTALL_PATH: &str = "/mnt/SDCARD/.tmp_update/startup/start-daemon.sh";
@@ -10,7 +11,7 @@ const SERVICE_PATH: &str = "./start-daemon.sh";
 /// * Moving all files to the correct locations.
 /// * Telling the operating system to start the daemon on boot.
 /// * Starting the daemon now.
-pub async fn install_daemon() -> Result<(), anyhow::Error> {
+pub async fn install_daemon() -> Result<(), DaemonError> {
     fs::copy(SERVICE_PATH, SERVICE_INSTALL_PATH).await?;
     start_daemon().await?;
     Ok(())
@@ -21,7 +22,7 @@ pub async fn install_daemon() -> Result<(), anyhow::Error> {
 /// * Stopping the daemon.
 /// * Telling the operating system to NOT start the daemon on boot.
 /// * Removing any stray files in the OS.
-pub async fn uninstall_daemon() -> Result<(), anyhow::Error> {
+pub async fn uninstall_daemon() -> Result<(), DaemonError> {
     stop_daemon().await?;
     match fs::remove_file(SERVICE_INSTALL_PATH).await {
         Ok(()) => Ok(()),
@@ -30,21 +31,27 @@ pub async fn uninstall_daemon() -> Result<(), anyhow::Error> {
     }
 }
 
-pub async fn restart_daemon() -> Result<(), anyhow::Error> {
+pub async fn restart_daemon() -> Result<(), DaemonError> {
     stop_daemon().await?;
     start_daemon().await?;
     Ok(())
 }
 
-pub async fn start_daemon() -> Result<(), anyhow::Error> {
+pub async fn start_daemon() -> Result<(), DaemonError> {
     let res = Command::new(SERVICE_PATH).output().await?;
     if res.status.success() {
         return Ok(());
     }
-    todo!()
+    let stdout = String::from_utf8_lossy(&res.stdout).into_owned();
+    let stderr = String::from_utf8_lossy(&res.stderr).into_owned();
+    Err(DaemonError::Subprocess {
+        process: res.status.code(),
+        stdout,
+        stderr,
+    })
 }
 
-pub async fn stop_daemon() -> Result<bool, anyhow::Error> {
+pub async fn stop_daemon() -> Result<bool, DaemonError> {
     let res = Command::new("killall")
         .arg("syncer-daemon")
         .output()
@@ -52,16 +59,31 @@ pub async fn stop_daemon() -> Result<bool, anyhow::Error> {
     Ok(res.status.success())
 }
 
-pub async fn reinstall_daemon() -> Result<(), anyhow::Error> {
+pub async fn reinstall_daemon() -> Result<(), DaemonError> {
     install_daemon().await?;
     restart_daemon().await?;
     Ok(())
 }
 
-pub async fn daemon_is_installed() -> Result<bool, anyhow::Error> {
+pub async fn daemon_is_installed() -> Result<bool, DaemonError> {
     match fs::try_exists(SERVICE_INSTALL_PATH).await {
         Ok(b) => Ok(b),
         Err(e) if e.kind() == io::ErrorKind::NotFound => Ok(false),
         Err(e) => Err(e.into()),
     }
+}
+
+#[derive(Debug, Error)]
+pub enum DaemonError {
+    #[error(
+        "Subprocess failed ({}). Stdout: {stdout}. Stderr: {stderr}", 
+        process.map(|n| n.to_string()).unwrap_or("<UNKNOWN>".to_owned())
+    )]
+    Subprocess {
+        process: Option<i32>,
+        stdout: String,
+        stderr: String,
+    },
+    #[error(transparent)]
+    Io(#[from] io::Error),
 }
